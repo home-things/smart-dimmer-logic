0a51f18f82d15605696665693e0e815c1f6b1769
Alex Mextner <invntrm@gmail.com>
2020-04-19 01:18:59 +0300

    make runnable

--- __main__.py
+++ __main__.py
@@ -12,36 +12,18 @@ class S(Enum):
 MIN = S.OFF
 MAX = S.MIDDLE
 
+def get_time(string):
+    [hour, minute] = string.split(':')
+    return now().replace(hour = int(hour), minute = int(minute))
+
 def now():
     return datetime.now()
 
 def get_sunset():
     return get_time(SUNSET_TIMES[now().month - 1])
 
-# sunset times for 12 months
-# generator here: ./get-sun-times.py
-SUNSET_TIMES = ['16:07', '17:02', '18:05', '19:08', '20:09', '21:03', '21:16', '20:34', '19:22', '18:03', '16:48', '16:01']
-
-state = S.STRIPE if now() >= get_sunset() else S.OFF
-latest_manual_action_time = None
-
-def get_time(string):
-    [hour, minute] = string.split(':')
-    return now().replace(hour = hour, minute = minute)
-
-def onSig(direction):
-    global latest_manual_action_time
-
-    dimm(+1 if direction == 'R' else -1)
-    latest_manual_action_time = now()
-    for i in [1, 2]: # lamp levels
-        ser.write(f"{i}{'H' if level >= {i} else 'L'}")
-
-def changed_manually_recently():
-    return (now() - latest_manual_action_time).seconds > 30 * 60 # todo
-
 # brighter / darken
-def dimm(delta):
+def update_state(delta):
     global state
     level = state.value + delta
     if level > MAX.value or level < MIN.value:
@@ -49,30 +31,50 @@ def dimm(delta):
     else:
         state = S(level)
 
+def dimm(state):
+    global latest_manual_action_time
+    latest_manual_action_time = now()
+    for i in [1, 2]: # lamp levels
+        ser.write('' + str(i) + ('H' if state.value >= i else 'L'))
+
+def changed_manually_recently():
+    return (now() - latest_manual_action_time).seconds > 30 * 60 # todo
+
+
+# sunset times for 12 months
+# generator here: ./get-sun-times.py
+SUNSET_TIMES = ['16:07', '17:02', '18:05', '19:08', '20:09', '21:03', '21:16', '20:34', '19:22', '18:03', '16:48', '16:01']
+
+state = S.STRIPE if now() >= get_sunset() else S.OFF
+dimm(state)
+latest_manual_action_time = datetime.min
 
 #
 # main
 #
 
 while True:
-    global state
-    command = ser.readString() # todo
+    command = ser.read_until('\0') # todo
 
     if command == "turn R":
-        onSig("R")
+        update_state(+1)
+        dimm(state)
     elif command == "turn L":
-        onSig("L")
+        update_state(-1)
+        dimm(state)
     if state == S.OFF:
-        None
+        pass
     elif state == S.STRIPE:
-        None
+        pass
     elif state == S.MIDDLE:
-        None
+        pass
 
     if not changed_manually_recently():
         if state == S.OFF and now() > get_time('08:40'):
-            dimm(+1)
+            update_state(+1)
+            dimm(state)
 
         if now() >= get_sunset():
-            dimm(+1)
+            update_state(+1)
+            dimm(state)
 
