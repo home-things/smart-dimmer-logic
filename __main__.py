4ef22b167a19374c62c817afe223e34b5f7d3c24
Alex Mextner <invntrm@gmail.com>
2020-04-19 15:19:29 +0300

    make works

--- __main__.py
+++ __main__.py
@@ -3,7 +3,13 @@ from enum import Enum
 import serial
 
 # fix rpi uart before: https://www.raspberrypi.org/documentation/configuration/uart.md
-ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=0, bytesize=serial.EIGHTBITS) # todo: rate, fix rpi uart
+ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1, bytesize=serial.EIGHTBITS)
+PRINT_SER = False
+ser.reset_output_buffer()
+ser.reset_input_buffer()
+ser.flush()
+
+print("connected to: " + ser.portstr)
 
 class S(Enum):
     OFF = 0
@@ -11,6 +17,20 @@ class S(Enum):
     MIDDLE = 2
 MIN = S.OFF
 MAX = S.MIDDLE
+
+def send(string):
+    ser.write(string.encode('utf-8'))
+    ser.flush()
+
+def recv():
+    return ser.readline().decode('utf-8').strip()
+    #line = ''
+    #for byte in ser.read():
+    #    char = chr(byte)
+    #    if PRINT_SER and char != '': print(char, end='')
+    #    line += char
+    #return line
+
 
 def get_time(string):
     [hour, minute] = string.split(':')
@@ -27,22 +47,23 @@ def update_state(delta):
     global state
     level = state.value + delta
     if level > MAX.value or level < MIN.value:
-        ser.write("vE") # error feedback
+        send('vB') # error feedback
     else:
         state = S(level)
 
+# brighter / darken
 def dimm(state):
     global latest_manual_action_time
     latest_manual_action_time = now()
     for i in [1, 2]: # lamp levels
-        ser.write('' + str(i) + ('H' if state.value >= i else 'L'))
+        send('' + str(i) + ('H' if state.value >= i else 'L'))
 
 def changed_manually_recently():
     return (now() - latest_manual_action_time).seconds > 30 * 60 # todo
 
 
 # sunset times for 12 months
-# generator here: ./get-sun-times.py
+# generator is here: ./get-sun-times.py
 SUNSET_TIMES = ['16:07', '17:02', '18:05', '19:08', '20:09', '21:03', '21:16', '20:34', '19:22', '18:03', '16:48', '16:01']
 
 state = S.STRIPE if now() >= get_sunset() else S.OFF
@@ -54,7 +75,9 @@ latest_manual_action_time = datetime.min
 #
 
 while True:
-    command = ser.read_until('\0') # todo
+    command = recv()
+    if not command: continue
+    print(f"command: '{command}'")
 
     if command == "turn R":
         update_state(+1)
@@ -62,12 +85,6 @@ while True:
     elif command == "turn L":
         update_state(-1)
         dimm(state)
-    if state == S.OFF:
-        pass
-    elif state == S.STRIPE:
-        pass
-    elif state == S.MIDDLE:
-        pass
 
     if not changed_manually_recently():
         if state == S.OFF and now() > get_time('08:40'):
--- test-receiver.py
+++ test-receiver.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 import serial
-import time
+#import time
 ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=0, bytesize=serial.EIGHTBITS)
+print("connected to: " + ser.portstr)
 
-print('send...')
-ser.write(2)
-print('start...')
+#print('send...')
+#ser.write('2H'.encode('utf-8'))
+#print('start...')
+
 while True:
-  for line in ser.readline():
-    if chr(line) != '':
-      print(chr(line), end='')
-      #time.sleep(1)
-      # ser.write(1)
+    for line in ser.read():
+        line = chr(line)
+        if line != '':
+            print(line, end='')
+            #time.sleep(1)
