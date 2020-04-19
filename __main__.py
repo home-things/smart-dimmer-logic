7bc93db87c3210c9d73ddc7aa1662518f85ed599
Alex Mextner <invntrm@gmail.com>
2020-04-19 15:50:34 +0300

    fix dimming logic

--- __main__.py
+++ __main__.py
@@ -3,13 +3,11 @@ from enum import Enum
 import serial
 
 # fix rpi uart before: https://www.raspberrypi.org/documentation/configuration/uart.md
+# timeout -- that's why readline() get full command string at once
 ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1, bytesize=serial.EIGHTBITS)
-PRINT_SER = False
-ser.reset_output_buffer()
-ser.reset_input_buffer()
-ser.flush()
+PRINT_SER = True
 
-print("connected to: " + ser.portstr)
+print(f"connected to: {ser.portstr}")
 
 class S(Enum):
     OFF = 0
@@ -19,11 +17,15 @@ MIN = S.OFF
 MAX = S.MIDDLE
 
 def send(string):
+    if PRINT_SER: print(f"send <<< {string}")
     ser.write(string.encode('utf-8'))
     ser.flush()
 
 def recv():
-    return ser.readline().decode('utf-8').strip()
+    line = ser.readline().decode('utf-8').strip()
+    if PRINT_SER and line != '': print(f"recv >>> {line}")
+    return line
+
     #line = ''
     #for byte in ser.read():
     #    char = chr(byte)
@@ -43,7 +45,7 @@ def get_sunset():
     return get_time(SUNSET_TIMES[now().month - 1])
 
 # brighter / darken
-def update_state(delta):
+def inc_state(delta):
     global state
     level = state.value + delta
     if level > MAX.value or level < MIN.value:
@@ -51,15 +53,19 @@ def update_state(delta):
     else:
         state = S(level)
 
+def set_state(next_state):
+    global state
+    state = new_state
+
 # brighter / darken
 def dimm(state):
     global latest_manual_action_time
     latest_manual_action_time = now()
     for i in [1, 2]: # lamp levels
-        send('' + str(i) + ('H' if state.value >= i else 'L'))
+        send(str(i) + ('H' if state.value >= i else 'L'))
 
 def changed_manually_recently():
-    return (now() - latest_manual_action_time).seconds > 30 * 60 # todo
+    return (now() - latest_manual_action_time).seconds <= 30 * 60 # todo
 
 
 # sunset times for 12 months
@@ -70,6 +76,8 @@ state = S.STRIPE if now() >= get_sunset() else S.OFF
 dimm(state)
 latest_manual_action_time = datetime.min
 
+print(f"Initial state: {state}")
+
 #
 # main
 #
@@ -77,21 +85,36 @@ latest_manual_action_time = datetime.min
 while True:
     command = recv()
     if not command: continue
-    print(f"command: '{command}'")
 
-    if command == "turn R":
-        update_state(+1)
+    if command in ["turn L", "turn R"]:
+        print(f"command: '{command}'")
+        inc_state(+1 if command == "turn R" else -1)
         dimm(state)
-    elif command == "turn L":
-        update_state(-1)
-        dimm(state)
+        print(f"has changed recently?: {changed_manually_recently()}")
+        print(f"state: {state}")
 
-    if not changed_manually_recently():
+    elif not changed_manually_recently():
         if state == S.OFF and now() > get_time('08:40'):
-            update_state(+1)
+            inc_state(+1)
             dimm(state)
+            print("dimmed up automatically: good morning")
 
-        if now() >= get_sunset():
-            update_state(+1)
+        elif now() >= get_sunset():
+            inc_state(+1)
             dimm(state)
+            print("dimmed up automatically: good evening")
+
+        elif now() >= get_sunset() + 3:
+            if state.value > S.STRIPE.value:
+                set_state(S.STRIPE)
+                dimm(state)
+                print("dimmed down automatically: good evening")
+
+        elif now() >= get_sunset() + 5:
+            if state.value > S.STRIPE.value:
+                set_state(S.OFF)
+                dimm(state)
+                print("dimmed down automatically: good night")
+
+        print(f"state: {state}")
 
